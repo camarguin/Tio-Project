eda5b7c2c5ca54ca6d41bd3cdcedcdf361063d13
Lucas Gerhardt de Camargo <lucasgcamargo@icloud.com>
2020-06-12 17:05:31 -0700

    Add project

---
+++ js/carousel.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('#carousel').carousel({
+    shift: 200,
+    dist: -100,
+    indicators: true,
+    duration: 400,
+  });
+});
+autoplay();
+function autoplay() {
+  $('.carousel').carousel('next');
+  setTimeout(autoplay, 3000);
+}
---
+++ js/slideQuotes.js
@@ -0,0 +1,45 @@
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("sld");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    i++;
+    setTimeout(showSlides, 2000);
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+// $(document).ready(function () {
+//     $('#slideshow').carousel({
+//         fullWidth: true,
+//         indicators: true,
+//     });
+//   });
+//   autoplay();
+//   function autoplay() {
+//     $('.carousel').carousel('next');
+//     setTimeout(autoplay, 3000);
+//   }
